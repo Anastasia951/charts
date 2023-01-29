1f2ef7bb93f0c7ced6e16aa8a85a129ae1663e45
Anastasia951 <n.ivanova504@gmail.com>
2023-01-29 23:57:41 +0300

    creating lines

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ const ROWS_COUNT = 5
 const PADDING = 40
 const VIEW_HEIGHT = DPI_HEIGHT - PADDING * 2
 
+const canvas = document.getElementById('chart')
+const data = [
+  [0, 0],
+  [200, 200],
+  [400, 50],
+  [600, 400]
+]
+
+
+function computeBoundaries(data) {
+  let min = data[0][1]
+  let max = data[0][1]
+  for (let [, y] of data) {
+    if (y < min) min = y
+    if (y > max) max = y
+  }
+
+  return [min, max]
+}
+
+const [yMin, yMax] = computeBoundaries(data)
+const yRatio = VIEW_HEIGHT / (yMax - yMin)
+
+
 function chart(canvas, data) {
   const ctx = canvas.getContext('2d')
   canvas.style.width = WIDTH + 'px'
@@ -15,12 +39,16 @@ function chart(canvas, data) {
 
   ctx.beginPath()
   let step = VIEW_HEIGHT / ROWS_COUNT
+  let textStep = (yMax - yMin) / ROWS_COUNT
+
   ctx.strokeStyle = '#bbb'
   ctx.font = '20px Arial'
   ctx.fillStyle = '#222'
+
   for (let i = 1; i <= ROWS_COUNT; i++) {
     const y = step * i
-    ctx.fillText(DPI_HEIGHT - y, 5, y + PADDING - 10)
+    const text = yMax - i * textStep
+    ctx.fillText(text, 5, y + PADDING - 10)
     ctx.moveTo(0, y + PADDING)
     ctx.lineTo(DPI_WIDTH, y + PADDING)
   }
@@ -32,16 +60,11 @@ function chart(canvas, data) {
   ctx.lineWidth = 4
   ctx.strokeStyle = 'violet'
   for (const [x, y] of data) {
-    ctx.lineTo(x, DPI_HEIGHT - PADDING - y)
+    ctx.lineTo(x, DPI_HEIGHT - PADDING - y * yRatio)
   }
   ctx.stroke()
   ctx.closePath()
 }
 
-const canvas = document.getElementById('chart')
-const data = [
-  [0, 0],
-  [200, 200],
-  [400, 50]
-]
+
 chart(canvas, data)
